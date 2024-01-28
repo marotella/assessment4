1e6ac0ce6fe4a0b7ec1a0acb10aa2a06fd12e276
marotella <michelle.a.rotella@gmail.com>
2024-01-28 15:27:47 -0500

    fortune feature added

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,17 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["I see great wealth in your future!", "Hardships are ahead but you will overcome!", "You will see the benefits of your hardwork soon!", "Remember to unplug your hair straighter this week.", "Your avocado will be ripe on Thursday."];
+      
+        // choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        console.log(randomIndex)
+        let randomFortune = fortunes[randomIndex];
+        console.log(randomFortune)
+      
+        res.status(200).send(randomFortune);
     }
 
 }
